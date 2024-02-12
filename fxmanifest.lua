947d9a099a08fdd388aa47d1536b5c53db6248bd
MrsBongmasta406 <111566632+MrsBongmasta406@users.noreply.github.com>
2024-02-12 16:22:18 -0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version ‘cerulean’
+game ‘gta5’
+author ‘Mrs.Bong’
+description ‘QBCore Hat Made By Kakarot’
+version ‘4.2.0’
+
+files {
+    'mp_f_freemode_01_qbcore.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_qbcore.meta'
